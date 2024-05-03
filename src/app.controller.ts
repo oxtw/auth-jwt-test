894b1854d4bb45f1c0ef6168583d879213e4736b
oxtw <weigertmiguel@hotmail.com>
2024-05-02 23:07:38 -0300

    Setup do Prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
@@ -7,11 +7,11 @@ export class AppController {
 
   @Get('/hello')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 
   @Post('/hello')
-  store(): string{
+  store(): string {
     return 'teste'
   }
 }
