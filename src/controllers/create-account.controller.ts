00dc8b4c78e12c2753f44d0be1c6dde5c9a9b640
oxtw <weigertmiguel@hotmail.com>
2024-05-06 12:17:51 -0300

    gerando hash da senha

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,15 +1,27 @@
-import { Body, ConflictException, Controller, Post } from '@nestjs/common'
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
-
+import { hash } from 'bcryptjs'
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
+  @HttpCode(201)
   async handle(@Body() body: any) {
     const { name, email, password } = body
 
-    const userWithSameEmail = this.prisma.user.findUnique({
+    if (!email) {
+      throw new BadRequestException('Email is required');
+    }
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
         email,
       },
@@ -21,11 +33,13 @@ export class CreateAccountController {
       )
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     })
   }
