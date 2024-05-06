66913c77a93d0a5da65cc915a0095d3d737b8b55
oxtw <weigertmiguel@hotmail.com>
2024-05-06 15:58:22 -0300

    Configurando autenticação JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
-import { ConfigModule } from '@nestjs/config';
-import { envSchema } from './prisma/env';
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './prisma/env'
 @Module({
-  imports:[ConfigModule.forRoot({
-    validate: env => envSchema.parse(env),
-    isGlobal: true,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
